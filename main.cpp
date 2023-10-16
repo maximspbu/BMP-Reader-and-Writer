758c271496bd48d7b8b2b7936c218ac43119da62
maxim <st119024@student.spbu.ru>
2023-10-16 21:51:35 +0000

    updated main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
+# define M_PI           3.14159265358979323846
 using namespace std;
 
 #pragma pack(push, 1) // выравнивание размера полей для структуры
@@ -12,7 +13,8 @@ struct BMPFileHeader{
     uint16_t reserved2{0};
     uint32_t offset_data{0};
 };
-
+#pragma pack(pop)
+#pragma pack(push, 1)
 struct BMPInfoHeader{
     uint32_t size{0};
     int32_t width{0};
@@ -26,79 +28,87 @@ struct BMPInfoHeader{
     uint32_t colors_used{0};
     uint32_t colors_important{0};
 };
-
+#pragma pack(pop)
 struct BMP{
-    BMPFileHeader fileHeader;
-    BMPInfoHeader infoHeader;
-    vector<char> matrix;
+    BMPFileHeader file_header;
+    BMPInfoHeader info_header;
 
-    void readBMP(const char* path){
+    vector<char> padding(int* row_stride){
+        *row_stride = info_header.width*3; // для считывания с учетом отступов
+        uint32_t new_stride = *row_stride;
+        while (new_stride%4!=0){
+            new_stride++;
+        }
+        vector<char> padding_row(new_stride-*row_stride);
+        return padding_row;
+    }
+
+    vector<char> read_bmp(const char* path){
         ifstream fs{path, ios_base::binary};
-        if (fs){
-            fs.read((char*)&fileHeader, sizeof(fileHeader));
-            fs.read((char*)&infoHeader, sizeof(infoHeader));
-            fs.seekg(fileHeader.offset_data, fs.beg); // перемещаем указатель на место в файле, где записаны пиксели
-            fileHeader.file_size = fileHeader.offset_data; // для подсчета размера файла мы пока считаем только размер заголовка
-            matrix.resize(infoHeader.width*infoHeader.height*3);
-            if (infoHeader.width%4==0){
+        if (fs.is_open()){
+            fs.read((char*)&file_header, sizeof(file_header));
+            fs.read((char*)&info_header, sizeof(info_header));
+            fs.seekg(file_header.offset_data, fs.beg); // перемещаем указатель на место в файле, где записаны пиксели
+            file_header.file_size = file_header.offset_data; // для подсчета размера файла мы пока считаем только размер заголовка
+            vector<char> matrix(info_header.width*info_header.height*3);
+            if (info_header.width%4==0){
                 fs.read((char*)matrix.data(), matrix.size());
-                fileHeader.file_size += (uint32_t)(matrix.size()); // размер файла уже с учетом вектора пикселей
+                file_header.file_size += (uint32_t)(matrix.size()); // размер файла уже с учетом вектора пикселей
             } else {
-                int row_stride = infoHeader.width*3; // для считывания с учетом отступов
-                uint32_t new_stride = row_stride;
-                while (new_stride%4!=0){
-                    new_stride++;
-                }
-                vector<char> padding_row(new_stride-row_stride);
-                for (int y = 0;y<infoHeader.height; ++y){
+                int row_stride = 0;
+                vector<char> padding_row = padding(&row_stride);
+                for (int y = 0;y<info_header.height; ++y){
                     fs.read((char*)(matrix.data() + row_stride * y), row_stride);
                     fs.read((char*)padding_row.data(), padding_row.size());
                 }
-                fileHeader.file_size += (uint32_t)(matrix.size()) + infoHeader.height*(uint32_t)(padding_row.size());
+                file_header.file_size += (uint32_t)(matrix.size()) + info_header.height*(uint32_t)(padding_row.size());
             }
             fs.close();
+            return matrix;
         }
+        cout << "No such file found" << endl;
+        exit(0);
     }
-    void rotateBMP_l(){
+    vector<char> rotate_bmp_l(vector<char> matrix){
         vector<char> copy_matrix(matrix.size());
+        int i, j, new_j, coords;
         for (int x = 0; x<matrix.size(); x++){
-            //int i = x/(3*infoHeader.width);
-            //int j = (x%(3*infoHeader.width))/3;
-            //int new_j = i;
-            //int new_i = infoHeader.width - 1 - j;
-            //int coords = x%3 + 3*(new_i*infoHeader.height+new_j);
-            //copy_matrix[coords] = matrix[x];
-            copy_matrix[x%3 + 3*(((x%(3*infoHeader.width))/3)*infoHeader.height+infoHeader.height - 1 - x/(3*infoHeader.width))] = matrix[x];
+            i = x/(3*info_header.width);
+            j = (x%(3*info_header.width))/3;
+            new_j = info_header.height - 1 - i;
+            coords = x%3 + 3*(j*info_header.height+new_j);
+            copy_matrix[coords] = matrix[x];
         }
-        swap(infoHeader.height, infoHeader.width);
-        matrix=copy_matrix;
+        swap(info_header.height, info_header.width);
+        return copy_matrix;
     }
 
-    void rotateBMP_r(){
+    vector<char> rotate_bmp_r(vector<char> matrix){
         vector<char> copy_matrix(matrix.size());
+        int i, j, new_i, coords;
         for (int x = 0; x<matrix.size(); x++){
-            copy_matrix[x%3 + 3*((infoHeader.width - 1 - (x%(3*infoHeader.width))/3)*infoHeader.height+x/(3*infoHeader.width))] = matrix[x];
+            i = x/(3*info_header.width);
+            j = (x%(3*info_header.width))/3;
+            new_i = info_header.width - 1 - j;
+            coords = x%3 + 3*(new_i*info_header.height+i);
+            copy_matrix[coords] = matrix[x];
         }
-        swap(infoHeader.height, infoHeader.width);
-        matrix = copy_matrix;
+        swap(info_header.height, info_header.width);
+        return copy_matrix;
     }
 
-    void writeBMP(const char *fname){
+    void write_bmp(const char *fname, vector<char> matrix){
         ofstream of{fname, ios_base::binary};
-        if (infoHeader.width%4==0){
-            of.write((const char*)&fileHeader, sizeof(fileHeader));
-            of.write((const char*)&infoHeader, sizeof(infoHeader));
+        if (info_header.width%4==0){
+            of.write((const char*)&file_header, sizeof(file_header));
+            of.write((const char*)&info_header, sizeof(info_header));
             of.write((const char*)matrix.data(), matrix.size());
         } else {
-                int row_stride = infoHeader.width*3; // считаем отступ заново, т.к. размеры bmp могли поменяться при повороте
-                uint32_t new_stride = row_stride;
-                while (new_stride%4!=0){
-                    new_stride++;
-                }
-                vector<char> padding_row(new_stride - row_stride);
-                of.write((const char*)&fileHeader, sizeof(fileHeader));
-                of.write((const char*)&infoHeader, sizeof(infoHeader));
-                for (int y = 0; y < infoHeader.height; ++y){
+                int row_stride = 0;
+                vector<char> padding_row = padding(&row_stride);
+                of.write((const char*)&file_header, sizeof(file_header));
+                of.write((const char*)&info_header, sizeof(info_header));
+                for (int y = 0; y < info_header.height; ++y){
                     of.write((const char*)(matrix.data() + row_stride * y), row_stride);
                     of.write((const char*)padding_row.data(), padding_row.size());
                 }
@@ -110,12 +120,12 @@ struct BMP{
         return 1. / exp(-(x*x+y*y)/(2*sigma*sigma));
     }
 
-    double *generate_coeff(int radius, double sigma){
-        double *coeff = new double[sizeof(double)*radius*radius];
+    vector<double> generate_coeff2(int radius, double sigma){
+        vector<double> coeff(sizeof(double)*radius*radius);
         double sum = 0;
         for (int i = 0; i<radius; i++){
             for (int j = 0; j<radius; j++){
-                coeff[i*radius+j] = gaussianModel(i-radius, j-radius/2, sigma);
+                coeff[i*radius+j] = gaussianModel(i-radius, j-radius/2, sigma) /(M_PI*2*sigma*sigma);
                 sum+=coeff[i*radius+j];
             }
         }
@@ -125,43 +135,36 @@ struct BMP{
         return coeff;
     }
 
-    void gauss(){
+    vector<char> gauss(vector<char> matrix){
+        vector<char> copy_matrix(matrix.size());
         double sigma = 1.0;
         int radius = 5;
         int b, g, r;
-        int coord=0;
-        double* coeff = generate_coeff(radius, sigma);
-        for (int i = 0; i < 2*infoHeader.width; i++){
-            for (int j = 0; j < 3*infoHeader.height; j+=3){
+        vector<double> coeff = generate_coeff2(radius, sigma);
+        for (int i = 0; i < 3*info_header.width; i++){
+            for (int j = 0; j < 3*info_header.height; j+=3){
                 b = g = r = 0;
                 for (int m = 0; m < radius; m++){
                     for (int n = 0; n < radius; n++){
-                        coord = infoHeader.width*(i+m)+j;
-                        if (coord+3*n<0 || coord+2+3*n>matrix.size()) continue;
-                        b += coeff[m*radius+n] * matrix[coord + 3*n];
-                        g += coeff[m*radius+n] * matrix[coord+1 + 3*n];
-                        r += coeff[m*radius+n] * matrix[coord+2 + 3*n];
-                        //b += coeff[m*radius+n] * matrix[(i+m)*infoHeader.width + (j+n)];
-                        //g += coeff[m*radius+n] * matrix[(i+m)*infoHeader.width + (j+1+n)];
-                        //r += coeff[m*radius+n] * matrix[(i+m)*infoHeader.width + (j+2+n)];
-                        //b += coeff[m * radius + n] * matrix[0 + (i+m)*infoHeader.width + (j + 3*n)];
-                        //g += coeff[m * radius + n] * matrix[1 + (i+m)*infoHeader.width + (j + 3*n)];
-                        //r += coeff[m * radius + n] * matrix[2 + (i+m)*infoHeader.width + (j + 3*n)];
+                        b += coeff[m * radius + n] * matrix[0 + ((i+m)*info_header.height + (j+3*n))];
+                        g += coeff[m * radius + n] * matrix[1 + ((i+m)*info_header.height + (j+3*n))];
+                        r += coeff[m * radius + n] * matrix[2 + ((i+m)*info_header.height + (j+3*n))];
                     }
                 }
-                matrix[i*infoHeader.width + j + 0] = b;
-                matrix[i*infoHeader.width + j + 1] = g;
-                matrix[i*infoHeader.width + j + 2] = r;
+                copy_matrix[(i*info_header.height + j) + 0] = b;
+                copy_matrix[(i*info_header.height + j) + 1] = g;
+                copy_matrix[(i*info_header.height + j) + 2] = r;
             }
         }
+        return copy_matrix;
     }
 };
 
 int main(){
     BMP bmp;
-    bmp.readBMP("dwsample-bmp-640.bmp");
-    //bmp.rotateBMP_r();
-    bmp.gauss();
-    bmp.writeBMP("sample2_out.bmp");
+    vector<char> data = bmp.read_bmp("sample2.bmp");
+    //vector<char> data_l = bmp.rotate_bmp_l(data);
+    vector<char> data_gauss = bmp.gauss(data);
+    bmp.write_bmp("sample2_out.bmp", data_gauss);
     return 0;
 }
